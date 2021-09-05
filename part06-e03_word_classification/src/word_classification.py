d2f3d18410246c9aece665078d0d4b2b9d562940
Manmohit10 <manmohit.10@gmail.com>
2021-09-06 03:08:23 +0600

    e03

--- part06-e03_word_classification/src/word_classification.py
+++ part06-e03_word_classification/src/word_classification.py
@@ -37,17 +37,75 @@ def load_english():
     return lines
 
 def get_features(a):
-    return np.array([[]])
+    alphas=str("abcdefghijklmnopqrstuvwxyzäö-")
+    print(alphas)
+    result=np.zeros((1,29))
+    for word in a:
+        word=word.lower()
+        al_ar=np.zeros((1,29))
+        i=0
+        for letter in alphas:            
+            if letter in word:
+                for w in word:
+                    if w==letter:
+                        al_ar[0,i]+=1
+                i=i+1
+                
+            else:
+                i+=1
+        """li=[word]
+        li2=np.array(li).reshape(1,1)
+        row_ar=np.hstack((al_ar))"""
+        result=np.vstack([result,al_ar])
+    result=result[1:,:]
+    print(result.shape)
+
+    return result
 
 def contains_valid_chars(s):
+    s=s.lower()
+    for letter in s:
+        if letter in alphabet_set:
+            continue
+        else:
+            return False
     return True
 
 def get_features_and_labels():
-    return np.array([[]]), np.array([])
+    fin=load_finnish()
+    Flist=[]
+    for f in fin:
+        f=f.lower()
+        if contains_valid_chars(f):
+            Flist.append(f)
+    
+    eng=load_english()
+    Elist=[]
+    for e in eng:
+        if e[0].islower():
+            e=e.lower()
+            if contains_valid_chars(e):
+                Elist.append(e)
+    F_ar=np.array(Flist)
+    Fin_feature=get_features(F_ar)
+    Fin_target=np.zeros((len(Flist),1))
+    print(Fin_feature[0:5])
+    E_ar=np.array(Elist)
+    Eng_feature=get_features(E_ar)
+    Eng_target=np.ones((len(Elist),1))
+    combined_feature=np.vstack([F_ar,E_ar])
+    combined_target=np.vstack([Fin_target,Eng_target])
+    
+    return combined_feature,combined_target
 
 
 def word_classification():
-    return []
+    X,y=get_features_and_labels()
+    model=MultinomialNB()
+    model = MultinomialNB()
+    cval = model_selection.KFold(n_splits=5, shuffle=True, random_state=0)
+    scores = cross_val_score(model, X, y, cv=cval)
+    return scores
 
 
 def main():
