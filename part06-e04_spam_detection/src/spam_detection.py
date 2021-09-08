0d08b414fa03390d449fd2c664a81330d466c740
Manmohit10 <manmohit.10@gmail.com>
2021-09-08 12:07:27 +0600

    ilu
    program 4

--- part06-e04_spam_detection/src/spam_detection.py
+++ part06-e04_spam_detection/src/spam_detection.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 
-def spam_detection(random_state=0, fraction=1.0):
+def spam_detection(random_state=0, fraction=0.1):
     with open("src/ham.txt.gz",'r') as h:
         ham=h.readlines()
         f=int(fraction*len(ham))
@@ -16,25 +16,23 @@ def spam_detection(random_state=0, fraction=1.0):
         f=int(fraction*len(spam))
         spam=spam[0:f]
     comb=ham+spam
-    #print(comb[0])
     vector=CountVectorizer()
     x=vector.fit_transform(comb)
     X=x.toarray()
-    Y=np.array([0]*len(ham)+[1]*len(spam))[:,np.newaxis]
-    print(Y.shape)
-    X_train,X_test,Y_train,Y_test=train_test_split(X,Y,random_state=random_state)
+    Y=np.array([0]*len(ham)+[1]*len(spam))
+    X_train,X_test,Y_train,Y_test=train_test_split(X,Y,random_state=random_state,train_size=0.75)
     model=MultinomialNB()
     model.fit(X_train,Y_train)
     predict=model.predict(X_test)
 
     acc=accuracy_score(predict,Y_test)
     misclassified=[x for x in range(0,Y_test.shape[0]) if predict[x]  !=Y_test[x] ]
-    print(misclassified)
 
     return (acc, X_test.shape[0], len(misclassified))
 
 def main():
-    accuracy, total, misclassified = spam_detection(0,0.1)
+    #spam_detection()
+    accuracy, total, misclassified = spam_detection()
     print("Accuracy score:", accuracy)
     print(f"{misclassified} messages miclassified out of {total}")
 
