4f41d9866c41e21d7b6c1c080b2b8b5a4603cc5a
Manmohit10 <manmohit.10@gmail.com>
2021-09-14 01:14:11 +0600

    part 5 and 6

--- part06-e05_plant_clustering/src/plant_clustering.py
+++ part06-e05_plant_clustering/src/plant_clustering.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import scipy
+from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score
 
 
 def find_permutation(n_clusters, real_labels, labels):
@@ -13,7 +16,14 @@ def find_permutation(n_clusters, real_labels, labels):
     return permutation
 
 def plant_clustering():
-    return 0.0
+    data=load_iris()
+    model=KMeans(3,random_state=0)
+    #x_train,x_test,y_train,y_test=train_test_split(data.data,data.target,random_state=0,train_size=0.8)
+    model.fit(data.data)
+    permutation = find_permutation(3, data.target, model.labels_)
+    new_labels = [ permutation[label] for label in model.labels_]   # permute the labels
+    #print("Accuracy score is", accuracy_score(data.target, new_labels))
+    return accuracy_score(data.target, new_labels)
 
 def main():
     print(plant_clustering())
--- part06-e06_nonconvex_clusters/src/nonconvex_clusters.py
+++ part06-e06_nonconvex_clusters/src/nonconvex_clusters.py
@@ -2,12 +2,73 @@
 
 import pandas as pd
 import numpy as np
+import scipy
 from sklearn.cluster import DBSCAN
 from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
 
+
+def find_permutation(n_clusters, real_labels, labels):
+    permutation=[]
+    for i in range(n_clusters):
+        idx = labels == i
+        # Choose the most common label among data points in the cluster
+        new_label=scipy.stats.mode(real_labels[idx])[0][0]
+        permutation.append(new_label)
+    return permutation
 
 def nonconvex_clusters():
-    return pd.DataFrame()
+    df=pd.read_csv("src/data.tsv",sep="\t")
+    #print(df.head())
+    X=df[['X1','X2']]
+    Y=df['y']
+    model=[]
+    acc=[]
+    clusters=[]
+    original_clusters=len(set(Y))
+    outliers=[]
+    
+    n=np.arange(0.05, 0.2, 0.05)
+    columns=['eps','Score','Clusters','Outliers']
+    result=pd.DataFrame(columns=columns)
+
+    for i in range(4):
+        model.append(DBSCAN(eps=n[i]))
+        model[i].fit(X)
+        y_predict=model[i].labels_
+        
+        df[i]=y_predict
+        mod_Y=df[df[i]!=-1]['y']
+        #idx=y_predict!=(-1)
+        #mod_Y_predict=y_predict[idx]
+        mod_Y_predict=df[df[i]!=-1][i]
+
+
+        cluster_temp=set(y_predict)
+        if (-1) in cluster_temp:
+            new_clusters=len(cluster_temp)-1
+            clusters.append(new_clusters)
+        else:
+            new_clusters=len(cluster_temp)
+            clusters.append(new_clusters)
+        if original_clusters==new_clusters:
+            acc.append(accuracy_score(mod_Y,mod_Y_predict))            
+        else:
+            acc.append(np.nan)
+        outliers.append(list(y_predict).count(-1))
+        result.loc[len(result.index)]=[n[i],acc[i],clusters[i],outliers[i]]
+    #permulation=find_permutation()
+    
+
+    new_label=[]    
+    fig,ax=plt.subplots(2,2)
+    m=0
+    for j in range(2):
+        for k in range(2):
+            ax[j,k].scatter(df['X1'],df['X2'], c=model[m].labels_)
+            m+=1
+    #plt.show()
+    return result
 
 def main():
     print(nonconvex_clusters())
