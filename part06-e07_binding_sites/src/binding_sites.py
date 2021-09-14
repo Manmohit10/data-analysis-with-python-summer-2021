328fd970660642cb98f3070a1ae4eb3a98dba682
Manmohit10 <manmohit.10@gmail.com>
2021-09-14 20:54:17 +0600

    Final submission

--- part06-e07_binding_sites/src/binding_sites.py
+++ part06-e07_binding_sites/src/binding_sites.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import numpy as np
+from sklearn import metrics
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import pairwise_distances
@@ -10,14 +11,38 @@ from matplotlib import pyplot as plt
 
 import seaborn as sns
 sns.set(color_codes=True)
+import scipy
 import scipy.spatial as sp
 import scipy.cluster.hierarchy as hc
 
+def find_permutation(n_clusters, real_labels, labels):
+    permutation=[]
+    for i in range(n_clusters):
+        idx = labels == i
+        # Choose the most common label among data points in the cluster
+        new_label=scipy.stats.mode(real_labels[idx])[0][0]
+        permutation.append(new_label)
+    return permutation
+
 def toint(x):
-    return 0
+    DNA=['A','C','G','T']
+    LI=[0,1,2,3]
+    #d=dict(zip("ACGT", range(4)))
+    result=dict(zip(DNA,LI))
+    return result[x]
 
 def get_features_and_labels(filename):
-    return (np.array([[]]), np.array([]))
+    df = pd.read_csv(filename, sep='\t')
+    #print(df.head())
+    mat = np.zeros((len(df.index),len(df.loc[2,'X'])))
+    for i in range(len(df.index)):
+        str=df.loc[i,'X']
+        row=[]
+        for w in str:
+            row.append(toint(w))
+        mat[i]=np.array(row)
+    #print(mat[0:10,:])
+    return (mat, df['y'])
 
 def plot(distances, method='average', affinity='euclidean'):
     mylinkage = hc.linkage(sp.distance.squareform(distances), method=method)
@@ -26,15 +51,29 @@ def plot(distances, method='average', affinity='euclidean'):
     plt.show()
 
 def cluster_euclidean(filename):
-    return 0.0
+    feature,data_label=get_features_and_labels(filename)
+    model=AgglomerativeClustering(linkage='average',affinity='euclidean')
+    model.fit(feature)
+    permutation=find_permutation(len(list(data_label.unique())),model.labels_,data_label)
+    perm_label=[permutation[label] for label in model.labels_]
+    acc=accuracy_score(data_label,perm_label)
+    return acc
 
 def cluster_hamming(filename):
-    return 0.0
+    model=AgglomerativeClustering(linkage='average',affinity='precomputed')
+    feature,data_label=get_features_and_labels(filename)
+    distance=pairwise_distances(feature,metric='hamming')
+    model.fit(distance)
+    permutation=find_permutation(len(list(data_label.unique())),model.labels_,data_label)
+    perm_label=[permutation[label] for label in model.labels_]
+    acc=accuracy_score(data_label,perm_label)
+    #plot(distance, "average", "hamming")
+    return acc
 
 
 def main():
     print("Accuracy score with Euclidean affinity is", cluster_euclidean("src/data.seq"))
     print("Accuracy score with Hamming affinity is", cluster_hamming("src/data.seq"))
-
+    #get_features_and_labels("src/data.seq")
 if __name__ == "__main__":
     main()
--- part06-e08_explained_variance/src/explained_variance.py
+++ part06-e08_explained_variance/src/explained_variance.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 
+import pandas as pd
+from sklearn.decomposition import PCA
+import numpy as np
+import matplotlib.pyplot as plt
 
 def explained_variance():
-    return [], []
+    df = pd.read_csv('src/data.tsv', sep='\t')
+    var = df.var(axis=0)
+    pca = PCA()
+    pca.fit(df)
+    return var, pca.explained_variance_
 
 def main():
     v, ev = explained_variance()
     print(sum(v), sum(ev))
+    print("The variances are:", " ".join([f"{x:.3f}" for x in v]))
+    print("The explained variances after PCA are:", " ".join([f"{x:.3f}" for x in ev]))
+    plt.plot(np.arange(1,11), np.cumsum(ev));
+    plt.show()
 
 if __name__ == "__main__":
     main()
